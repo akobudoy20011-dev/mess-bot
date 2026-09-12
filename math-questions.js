c36adf42c36c41d65a0c49012bae941ece350b32
akobudoy20011-dev <akobudoy20011@gmail.com>
2026-09-12 22:32:59 +0800

    Create math-questions.js

---
+++ math-questions.js
@@ -0,0 +1,54 @@
+module.exports = [
+  // ADDITION
+  { type: "addition", min: 1, max: 20 },
+  { type: "addition", min: 10, max: 50 },
+  { type: "addition", min: 20, max: 100 },
+  { type: "addition", min: 50, max: 200 },
+  { type: "addition", min: 100, max: 500 },
+
+  // SUBTRACTION
+  { type: "subtraction", min: 1, max: 20 },
+  { type: "subtraction", min: 10, max: 50 },
+  { type: "subtraction", min: 20, max: 100 },
+  { type: "subtraction", min: 50, max: 200 },
+  { type: "subtraction", min: 100, max: 500 },
+
+  // MULTIPLICATION
+  { type: "multiplication", min: 2, max: 5 },
+  { type: "multiplication", min: 2, max: 10 },
+  { type: "multiplication", min: 2, max: 12 },
+  { type: "multiplication", min: 5, max: 20 },
+  { type: "multiplication", min: 10, max: 25 },
+
+  // DIVISION
+  { type: "division", min: 2, max: 5 },
+  { type: "division", min: 2, max: 10 },
+  { type: "division", min: 2, max: 12 },
+  { type: "division", min: 5, max: 20 },
+  { type: "division", min: 10, max: 25 },
+
+  // MIXED EASY
+  { type: "mixed", min: 1, max: 20 },
+  { type: "mixed", min: 1, max: 30 },
+  { type: "mixed", min: 1, max: 50 },
+
+  // MIXED MEDIUM
+  { type: "mixed", min: 10, max: 50 },
+  { type: "mixed", min: 20, max: 100 },
+  { type: "mixed", min: 50, max: 150 },
+
+  // SQUARES
+  { type: "square", min: 2, max: 10 },
+  { type: "square", min: 5, max: 15 },
+  { type: "square", min: 10, max: 20 },
+
+  // PERCENTAGES
+  { type: "percentage", min: 10, max: 50 },
+  { type: "percentage", min: 50, max: 200 },
+  { type: "percentage", min: 100, max: 500 },
+
+  // ORDER OF OPERATIONS
+  { type: "order", min: 1, max: 10 },
+  { type: "order", min: 5, max: 20 },
+  { type: "order", min: 10, max: 30 }
+];
