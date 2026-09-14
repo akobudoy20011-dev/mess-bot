93ae7de5f8c9ffabcc17e4e0ddf969ac9f8128d1
akobudoy20011-dev <akobudoy20011@gmail.com>
2026-09-14 22:51:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -634,7 +634,6 @@ async function handleMessage(api, event) {
   } catch (error) {
     console.error("RPG character handler failed:", error);
   }
-}
 
   // -------------------------------------------------------------------------
   // ACTIVE GAME RESPONSES
