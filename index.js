2c90c5257bc9513d8d6da224444ef59eb107d572
akobudoy20011-dev <akobudoy20011@gmail.com>
2026-09-17 04:13:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ const {
   handleDebugCommand,
 } = require("./debug");
 
+const {
+  startCleanupScheduler,
+} = require("./cleanup");
+
 // ============================================================
 // PICTURES
 // ============================================================
