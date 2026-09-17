494853ad36fed227a72943b26a41dca039186a5c
akobudoy20011-dev <akobudoy20011@gmail.com>
2026-09-18 00:26:38 +0800

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -191,6 +191,16 @@ async function searchYouTube(query) {
     `[YouTube] Found: ${video.title || cleanQuery}`
   );
 
+  /*
+   * Return the YouTube video ID as well.
+   *
+   * lyrics.js uses this to look for:
+   *
+   * data/lyrics/<videoId>.lrc
+   *
+   * This is more reliable than matching only the
+   * YouTube title.
+   */
   return {
     title:
       video.title ||
@@ -198,6 +208,10 @@ async function searchYouTube(query) {
 
     url:
       video.url,
+
+    videoId:
+      video.videoId ||
+      null,
 
     duration:
       video.duration ||
